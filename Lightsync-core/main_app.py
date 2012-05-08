b6ffd47498778bc4691b1928de33a0a7bdd565bc
Piotr Lesniak <pita_atip@tlen.pl>
2012-05-08 19:51:52 +0200

    restoring added

--- Lightsync-core/main_app.py
+++ Lightsync-core/main_app.py
@@ -9,7 +9,8 @@ please use this as sudo python main_app
 import pyinotify
 from os import path, listdir
 from time import sleep
-from recipie import backup_files
+from recipie import backup_files, restore_files
+import sys
 
 
 def parseConfig(conf_path):
@@ -27,27 +28,46 @@ class MyEventHandler(pyinotify.ProcessEvent):
             # give mounted device time to mount
             sleep(0.5)
             l = [name for name in listdir(event.pathname) if name == '.lightsync']
-            i=0
+            i = 0
             
             # if device is empty or big as it didn't mount in moment, wait for some time
-            while len(l) == 0 and i<5:
+            while len(l) == 0 and i < 5:
                 sleep(0.3)
                 l = [name for name in listdir(event.pathname)]
-                i+=1
+                i += 1
             
             if len(l) == 1 :
-                print "Mounted drive is LightSync backup drive"
-                # get list of catalogs to backup
-                for bkup_f in parseConfig(path.join(event.pathname, l[0])):
-                    print "Backuping %s in %s"%(bkup_f,  path.join(event.pathname, 'LighSyncBackup'))
-                    backup_files(bkup_f, path.join(event.pathname, 'LighSyncBackup'))
-            print 'backup complete...'
-                
+                print "Mounted drive is LightSync backup drive\n"
+                try:
+                    mode = int(raw_input('Choose:\n1. Backup files\n2. Restore file\n'))
+                except ValueError:
+                    print "Not a number"
+                    exit
+                if mode == 1:
+                    # get list of catalogs to backup
+                    for bkup_f in parseConfig(path.join(event.pathname, l[0])):
+                        backup_files(bkup_f, path.join(event.pathname, 'LighSyncBackup'))
+                    print 'backup complete...'
+                if mode == 2:
+                    restore_path = raw_input('Enter path to restore backup:\n')
+                    for restr_f in listdir(path.join(event.pathname, 'LighSyncBackup')):
+                        restore_files(path.join(event.pathname, 'LighSyncBackup', restr_f), restore_path)
+                    print "Restoring files complete"
+                else:
+                    print "wrong number"
+                    exit
+  
+             
             
 def main():
-    # watch manager
+    if len(sys.argv) == 2:
+        with open(sys.argv[1]) as conf_file:
+            for line in conf_file:
+                MOUNT_DRIVE = line.strip()
+    else:
+        MOUNT_DRIVE = '/media/' 
     wm = pyinotify.WatchManager()
-    wm.add_watch('/media/', pyinotify.ALL_EVENTS, rec=False)
+    wm.add_watch(MOUNT_DRIVE, pyinotify.ALL_EVENTS, rec=False)
 
     # event handler
     eh = MyEventHandler()
--- Lightsync-core/recipie.py
+++ Lightsync-core/recipie.py
@@ -9,6 +9,69 @@ import sys,os, shutil, filecmp
 MAXVERSIONS=100
 BAKFOLDER = '.bak'
 
+def restore_files(tree_top, bakdir_name=BAKFOLDER):
+    top_dir = os.path.basename(tree_top)
+    tree_top += os.sep
+    
+    for dir, subdirs, files in os.walk(tree_top):
+
+        if os.path.isabs(bakdir_name):
+            relpath = dir.replace(tree_top,'')
+            backup_dir = os.path.join(bakdir_name, top_dir, relpath)
+        else:
+            backup_dir = os.path.join(dir, bakdir_name)
+
+        if not os.path.exists(backup_dir):
+            os.makedirs(backup_dir)
+        
+        print 'Restoring files in: ' + backup_dir 
+        # To avoid recursing into sub-directories
+        subdirs[:] = [d for d in subdirs if d != bakdir_name]
+        files_set = set()
+        for f in files:
+            files_set.add(f[:-3])
+        for f in files_set:
+            filepath = os.path.join(dir, f)
+            destpath = os.path.join(backup_dir, f)
+            # Check existence of previous versions
+            last_backup= None
+            for index in xrange(MAXVERSIONS):
+                backup = '%s.%2.2d' % (filepath, index)
+                if os.path.exists(backup):
+                    last_backup = backup
+                elif last_backup != None:
+                    break
+            try:
+                if not os.path.exists(backup):
+                    print 'Copying %s to %s...' % (last_backup, destpath)
+                    shutil.copy(last_backup, destpath)
+            except (OSError, IOError), e:
+                pass        
+            abspath = os.path.abspath(filepath)
+
+#                
+#                if index > 0:
+#                    # No need to backup if file and last version
+#                    # are identical
+#                    old_backup = '%s.%2.2d' % (destpath, index-1)
+#                    if not os.path.exists(old_backup): break
+#                    abspath = os.path.abspath(old_backup)
+#                    
+#                    try:
+#                        if os.path.isfile(abspath) and filecmp.cmp(abspath, filepath, shallow=False):
+#                            continue
+#                    except OSError:
+#                        pass
+#                
+#                try:
+#                    if not os.path.exists(backup):
+#                        print 'Copying %s to %s...' % (filepath, backup)
+#                        shutil.copy(filepath, backup)
+#                except (OSError, IOError), e:
+#                    pass
+
+    
+
 def backup_files(tree_top, bakdir_name=BAKFOLDER):
     """ Directory back up function. Takes the top-level
     directory and an optional backup folder name as arguments.
