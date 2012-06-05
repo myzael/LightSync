a74597f91ca5b629493eab856992738ee209ba7c
Jakub Mozgawa <jakub.mozgawa@gmail.com>
2012-06-05 19:42:38 +0200

    working gui loop

--- Lightsync-core/main_app.py
+++ Lightsync-core/main_app.py
@@ -19,7 +19,6 @@ def parseConfig(conf_path):
         for line in f:
             yield line.strip()
 
-
 class MyEventHandler(pyinotify.ProcessEvent):
     def __init__(self,icon):
         pyinotify.ProcessEvent.__init__(self)
@@ -43,26 +42,47 @@ class MyEventHandler(pyinotify.ProcessEvent):
             if len(l) == 1 :
                 print "Mounted drive is LightSync backup drive\n"
                 self.icon.show_message("Great!", "Mounted drive is LightSync backup drive")
-                try:
-                    mode = int(raw_input('Choose:\n1. Backup files\n2. Restore file\n'))
-                except ValueError:
-                    print "Not a number"
-                if mode == 1:
-                    # get list of catalogs to backup
-                    for bkup_f in parseConfig(path.join(event.pathname, l[0])):
-                        backup_files(bkup_f, path.join(event.pathname, 'LighSyncBackup'))
-                    print 'backup complete...'
-                elif mode == 2:
-                    restore_path = raw_input('Enter path to restore backup or type \"default\" to choose previous locations:\n')
-                    if restore_path == "default" : 
-                        for restr_f in parseConfig(path.join(event.pathname, l[0])):
-                            backup_files(path.join(event.pathname, 'LighSyncBackup', path.split(restr_f)[-1]), path.dirname(restr_f))
-                    else: 
-                        for restr_f in listdir(path.join(event.pathname, 'LighSyncBackup')):
-                            backup_files(path.join(event.pathname, 'LighSyncBackup', restr_f), restore_path)
-                    print "Restoring files complete"
-                else:
-                    print "wrong number"
+                #try:
+                #    mode = int(raw_input('Choose:\n1. Backup files\n2. Restore file\n'))
+                #except ValueError:
+                #    print "Not a number"
+                #e = Example()
+                #mode = e.result
+                msgBox = QtGui.QMessageBox()
+                msgBox.setWindowTitle('Choose action')
+                msgBox.setText('Do you want to backup or restore?')
+                btnB   = QtGui.QPushButton('Backup')
+                msgBox.addButton(btnB, QtGui.QMessageBox.YesRole)
+                btnR   = QtGui.QPushButton('Restore')
+                msgBox.addButton(btnR, QtGui.QMessageBox.NoRole)
+                btnC   = QtGui.QPushButton('Cancel')
+                msgBox.addButton(btnC, QtGui.QMessageBox.RejectRole)
+                msgBox.exec_();                
+
+            if msgBox.clickedButton() == btnB:
+                mode = 1
+            elif msgBox.clickedButton() == btnR:
+                mode = 2
+            else:
+                mode = 0
+
+            if mode == 1:
+                # get list of catalogs to backup
+
+                for bkup_f in parseConfig(path.join(event.pathname, l[0])):
+                    backup_files(bkup_f, path.join(event.pathname, 'LighSyncBackup'))
+                print 'backup complete...'
+                self.icon.show_message('Completed','Backup complete!')
+            elif mode == 2:
+                #restore_path = raw_input('Enter path to restore backup or type \"default\" to choose previous locations:\n')
+#                    if restore_path == "default" : 
+                for restr_f in parseConfig(path.join(event.pathname, l[0])):
+                    backup_files(path.join(event.pathname, 'LighSyncBackup', path.split(restr_f)[-1]), path.dirname(restr_f))
+                #else: 
+                   # for restr_f in listdir(path.join(event.pathname, 'LighSyncBackup')):
+                   #     backup_files(path.join(event.pathname, 'LighSyncBackup', restr_f), restore_path)
+                print "Restoring files complete"
+                self.icon.show_message('Completed','Restore complete!')
 
 
 class SystemTrayIcon(QtGui.QSystemTrayIcon):
@@ -74,13 +94,15 @@ class SystemTrayIcon(QtGui.QSystemTrayIcon):
         self.setContextMenu(self.menu)
         
     def show_message(self,title,content):
-        QtCore.QTimer.singleShot(100, (lambda: self.showMessage(title,content)))
+        QtCore.QTimer.singleShot(10, (lambda: self.showMessage(title,content)))
     def show(self):
         QtGui.QSystemTrayIcon.show(self)
         self.show_message('Hello','This is LightSync')
             
 def main():
     app = QtGui.QApplication(sys.argv)
+    app.setQuitOnLastWindowClosed(False)
+
     style = app.style()
     icon = QtGui.QIcon(style.standardPixmap(QtGui.QStyle.SP_DriveHDIcon))
     trayIcon = SystemTrayIcon(icon)
@@ -108,7 +130,9 @@ def main():
     timer.start(100)
 
     trayIcon.show()
-    sys.exit(app.exec_())
+    
+    app.exec_()
+    #sys.exit(app.exec_())
 
 def quick_check(notifier):
     assert notifier._timeout is not None, 'Notifier must be constructed with a short timeout'
